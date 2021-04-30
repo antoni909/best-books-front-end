6c779d7d0b4d4d65e0a31dc41336c7fe06544af1
antoni <lorenzo.antonio.090@gmail.com>
2021-04-29 21:09:05 -0700

    wip

--- src/MyFavoriteBooks.js
+++ src/MyFavoriteBooks.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container'
+import Carousel from 'react-bootstrap/Carousel'
 import './MyFavoriteBooks.css';
 import { withAuth0 } from '@auth0/auth0-react';
 import axios from 'axios';
@@ -9,11 +10,8 @@ import axios from 'axios';
 class MyFavoriteBooks extends React.Component {
 constructor(props){
   super(props);
-  this.state = {
-    listOfBooks: [],
-  }
+  this.state = {}
 }
-  
 componentDidMount(){
   this.getListOfBooks();
 }
@@ -23,27 +21,44 @@ getListOfBooks = async() => {
   const books = await axios.get(`${SERVER}/books`,{params: {email: this.props.auth0.user.email,}});
 
   this.setState({
-    listOfBooks: books,
+    listOfBooks: books.data,
   })
-    console.log('response: ',books);
-    console.log('books: ', books)
+    // console.log('books arr: ', this.state.listOfBooks)
+    //map books.data if length > 0
 }
 
   render() {
     // eslint-disable-next-line no-unused-vars
     const { user, isAuthenticated } = this.props.auth0;
-    console.log(this.props.listOfBooks);
+    console.log(this.state.listOfBooks && this.state.listOfBooks[0], isAuthenticated);
     return(
+  
+      <>
+      {(isAuthenticated)?
+        <Container>
+          <Carousel>
+            {this.props.this.state.listOfBooks[0].map( (element) => {
 
+              return(
+                <Carousel.Item key={element._id} interval={5000}>
+                  <img
+                    className="d-block w-100"
+                    src={''}
+                    alt={element.description}
+                  />
+                  <Carousel.Caption>
+                    <h1>{element.name}</h1>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              );
 
-      <Jumbotron>
-        <h1>My Favorite Books</h1>
-        <p>
-          This is a collection of my favorite books
-        </p>
-      </Jumbotron>
+            })}
+          </Carousel>
+        </Container> 
+        : 'Here we will list your favorite books'
+      }
+    </>
 
-      
     )
   }
 }
