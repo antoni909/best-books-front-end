b58015770289794d9fc12f3c490fe488fd0d1f20
antoni <lorenzo.antonio.090@gmail.com>
2021-05-04 21:45:32 -0700

    debug auto-refresh error

--- src/DeleteBook.js
+++ src/DeleteBook.js
@@ -1,32 +1,22 @@
 import axios from "axios";
 import React from "react";
 import Button from "react-bootstrap/Button";
-// import axios from "axios";
+import './DeleteBook.css'
 
 class DeleteBook extends React.Component {
 
   handleClick = (e) => {
-
-    console.log('All the props available: ', this.props);
     e.preventDefault();
-
     axios
       .delete(`${process.env.REACT_APP_BACK_END}/books/${this.props.bookId}?user=${this.props.email}`)
       .then((response) => {
-        console.log("server res: ", response);
-        console.log('book was deleted');
-        
         let newBookList = this.props.bookList;
+        // newBookList = newBookList.filter(x => x._id !== this.props.bookId);
+        this.props.updateList(response.data);
+        console.log('1  -  new Booklist from server: ', newBookList)
+        alert('Congratulations, your book was deleted');
 
-        newBookList = newBookList.filter(x => x._id !== this.props.bookId);
-
-        console.log('the new book list', newBookList);
-  
-        this.props.updateList(newBookList);
-
-        alert('Congratulations, your book was deleted')
       });
-      
   };
 
   render() {
